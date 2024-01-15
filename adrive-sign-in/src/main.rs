27ff2799cbf2ceaa77934443fff988579b96ab52
dongruixiao <dongruixiao@gmail.com>
2024-01-15 22:32:20 +0800

    add sign-in backend

--- adrive-sign-in/src/main.rs
+++ adrive-sign-in/src/main.rs
@@ -1,7 +1,9 @@
 use adrive_api::data_structures::auth::*;
 use adrive_api::data_structures::Request;
 use axum::routing::{get, post};
+use axum::Json;
 use axum::Router;
+use serde::Deserialize;
 use std::env;
 
 async fn sid() -> String {
@@ -14,13 +16,27 @@ async fn sid() -> String {
     resp.sid
 }
 
-async fn status() -> () {}
+#[derive(Deserialize)]
+struct AuthCodePayload {
+    auth_code: String,
+}
+
+async fn token(Json(payload): Json<AuthCodePayload>) -> Json<GetAccessTokenResponse> {
+    let client_id = env::var("ADRIVE_CLIENT_ID").unwrap();
+    let client_secret = env::var("ADRIVE_CLIENT_SECRET").unwrap();
+    let resp =
+        GetAccessTokenRequest::new(&client_id, &client_secret, Some(&payload.auth_code), None)
+            .dispatch(None, None)
+            .await
+            .unwrap();
+    Json(resp)
+}
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/sid", get(sid))
-        .route("/status", get(status));
+        .route("/token", post(token));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:10024")
         .await
         .unwrap();
