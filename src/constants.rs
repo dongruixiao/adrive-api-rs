b5296f3ceb9de0737061e42266f5587f528aea85
dongruixiao <dongruixiao@gmail.com>
2024-04-02 23:03:09 +0800

    update SELF_HOSTING_SERVER

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
 pub(crate) const ADRIVE_OPENAPI_DOMAIN: &str = "https://openapi.alipan.com";
-pub(crate) const SELF_HOSTING_SERVER: &str = "https://adrive-sign-in.onrender.com";
+pub(crate) const SELF_HOSTING_SERVER: &str = "https://adrive-sign-in.hipster.workers.dev";
 
 pub(crate) const MAX_BATCH_SIZE: usize = 100;
 pub(crate) const MAX_CONCURRENCY: usize = 10;
